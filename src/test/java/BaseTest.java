d88925166878b5499196b45326e9afe93f80d0e3
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2014-12-08 22:58:57 -0600

    Extracted logic from before and after to base test class.

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,38 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    protected static WebDriver driver;
+
+    @BeforeClass
+    public static void beforeAll() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        System.out.println("BEFORE CLASS IS EXECUTED");
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        driver.quit();
+        System.out.println("AFTER ALL TESTS EXECUTED");
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        driver.get("http://devck-cms.sourceforge.net/ecm/");
+        System.out.println("BEFORE TEST EXECUTED");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.manage().deleteAllCookies();
+        System.out.println("AFTER TEST EXECUTED");
+    }
+
+}
